e6e35e2d43b50359fb1d641dbf0cdd894d8e9a0b
Neel Kapadia <neel.kapadia10@gmail.com>
2018-04-16 21:12:39 -0400

    module 1 done

--- scripts/recom.py
+++ scripts/recom.py
@@ -8,7 +8,18 @@ def get_rows_by_category(df, category):
 
 def get_checkin_count_per_venue_in_category(category_rows):
 	#get check incount of each venue with category coffee shop
-	venue_group = pd.DataFrame({'count' : category_rows.groupby(['venueid', 'latitude', 'longitude']).size()}).reset_index()
+	temp = category_rows
+	time = pd.to_datetime(temp['utctime'])
+	hour = []
+	for i in time:
+		hour.append(i.hour)
+	temp.is_copy = False
+	temp['hour'] = hour
+	category_rows.is_copy = False
+	category_rows["count"] =""
+	category_rows["avg_time"] =""
+	venue_group = temp.groupby(['venueid','longitude', 'latitude'])['hour'].agg({'count':'count', 'avg_time': 'mean'}).reset_index()
+	#venue_group = pd.DataFrame({'count' : temp.groupby(['venueid', 'latitude', 'longitude']).size()}).reset_index()
 	venue_group = venue_group.sort_values(['count'], ascending=False)
 	return venue_group
 
@@ -44,7 +55,7 @@ def calculate_p_go(count_venue, count_user_venue):
 	return p_go
 	#print(p_go)
 
-def calculate_checkins_within_venue_radius(venue_group, user_rows):
+def calculate_p_close(venue_group, user_rows):
 	#total number of checkins of each user
 	x = 0
 	d = []
@@ -61,13 +72,36 @@ def calculate_checkins_within_venue_radius(venue_group, user_rows):
 		d = []
 		user_rows = user_rows.loc[user_rows['distance'] < (maxi - mini)/3]
 		p_close.append(sum(user_rows['count'])/denom)
-	print(p_close[:10])
+	return p_close
 		#user_rows = user_rows['userid', 'venueid', 'latitude', 'longitude', 'count']
 		# user_rows = user_rows.drop(labels = ['distance'], axis = 1)
 		# print(user_rows.head())
 
-		
+def calculate_p_like(category_rows, venue_group, time):
+	denom = sum(venue_group['count'])
+	avg_time = pd.DataFrame({'average' : category_rows.groupby(['userid','venueid'])['hour'].mean()}).reset_index()
+	p_like = []
+	for index, venue in venue_group.iterrows():
+		num = venue['count']
+		initial_p_like = (num / denom)
+		time_diff = abs(time - venue['avg_time'])
+		if time_diff <= 3:
+			penalized_p_like = initial_p_like
+		elif time_diff <= 6:
+			penalized_p_like = initial_p_like * 0.8
+		elif time_diff <= 9:
+			penalized_p_like = initial_p_like * 0.6
+		elif time_diff <= 12:
+			penalized_p_like = initial_p_like * 0.4
+		else:
+			penalized_p_like = initial_p_like * 0.2
+		p_like.append(penalized_p_like)
+	return p_like
 	#distance between venue coordinates and each of user checkin, filter checkins using distance threshold and count #
+
+def suggestions(p_like, p_close):
+	p = []
+	
 
 def haversine_dist(lat1, lon1, lat2, lon2):
 	"""Calculate the Haversine distance between two geo co-ordiantes."""
@@ -83,6 +117,7 @@ if __name__ == '__main__':
 	df = pd.read_csv("../data/data-ny.csv", sep = ',', header=None, names =  ['userid', 'venueid', 'venuecatid', 'venuecatname','latitude','longitude','timezone','utctime'])
 	category = "Coffee Shop"
 	user = 642
+	time = 16
 
 	category_rows = get_rows_by_category(df, category)
 	venue_group = get_checkin_count_per_venue_in_category(category_rows)
@@ -90,5 +125,9 @@ if __name__ == '__main__':
 	user_venue_group = get_sorted_user_checkin_count_at_venues(category_rows)
 	count_user_venue = get_number_venues_visited_by_user(user_venue_group, user)
 	user_rows = get_all_user_checkins(df, user)
-	calculate_checkins_within_venue_radius(venue_group, user_rows)
-
+	p_go = calculate_p_go(count_venue, count_user_venue)
+	p_close = calculate_p_close(venue_group, user_rows)
+	p_like = calculate_p_like(category_rows, venue_group, time)
+	print(p_go)
+	print(p_close)
+	print(p_like)
